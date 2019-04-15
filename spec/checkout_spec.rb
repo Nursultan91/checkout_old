abf1a2101b48f96057ece3812868c5bc24a2aa9c
iamrealnurs <nurs567@yandex.ru>
2019-04-16 00:57:52 +0600

    First test Work

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -5,24 +5,24 @@ describe Checkout do
   subject { Checkout.new }
   it "prints current total" do
     expect { subject }.to output(
-                              <<-EOT.gsub(/^\s+/, '')
-        Input:
-        Output: $4.35
-                          EOT
-                          ).to_stdout
-  end
-
-  describe "#add" do
-    it "adds drinks to buyer bill with code" do
-      subject.add("PC")
-      subject.add("CC")
-      subject.add("CC")
-      expect { subject.total }.to output(
         <<-EOT.gsub(/^\s+/, '')
-        Input: PC CC CC
+        Input:
         Output: $4.35
         EOT
         ).to_stdout
-    end
   end
+
+  # describe "#add" do
+  #   it "adds drinks to buyer bill with code" do
+  #     subject.add("PC")
+  #     subject.add("CC")
+  #     subject.add("CC")
+  #     expect { subject.total }.to output(
+  #       <<-EOT.gsub(/^\s+/, '')
+  #       Input: PC CC CC
+  #       Output: $4.35
+  #       EOT
+  #       ).to_stdout
+  #   end
+  # end
 end
