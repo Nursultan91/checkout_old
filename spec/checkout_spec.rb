6f6dc33a13f42cb9d8c7863fda3eb2fa644fc33d
iamrealnurs <nurs567@yandex.ru>
2019-04-16 00:59:13 +0600

    Second test Work

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -12,17 +12,17 @@ describe Checkout do
         ).to_stdout
   end
 
-  # describe "#add" do
-  #   it "adds drinks to buyer bill with code" do
-  #     subject.add("PC")
-  #     subject.add("CC")
-  #     subject.add("CC")
-  #     expect { subject.total }.to output(
-  #       <<-EOT.gsub(/^\s+/, '')
-  #       Input: PC CC CC
-  #       Output: $4.35
-  #       EOT
-  #       ).to_stdout
-  #   end
-  # end
+  describe "#add" do
+    it "adds drinks to buyer bill with code" do
+      subject.add("PC")
+      subject.add("CC")
+      subject.add("CC")
+      expect { subject.total }.to output(
+        <<-EOT.gsub(/^\s+/, '')
+        Input: PC CC CC
+        Output: $4.35
+        EOT
+        ).to_stdout
+    end
+  end
 end
--- src/main.rb
+++ src/main.rb
@@ -2,7 +2,5 @@ require_relative 'checkout'
 
 ewq = Checkout.new
 5.times { ewq.add("asd") }
-ewq.total
 
 dsa = Checkout.new
-dsa.total
